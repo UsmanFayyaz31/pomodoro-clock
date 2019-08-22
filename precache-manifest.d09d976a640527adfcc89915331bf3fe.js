2f65c2cd054f0c7b6d48b38d1b60317442a49001
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2019-08-22 16:54:01 +0500

    Updates

---
+++ precache-manifest.d09d976a640527adfcc89915331bf3fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ecad7f270eefcb2086fe0c1e935bf8",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "960bf991070e212c4203",
+    "url": "/pomodoro-clock/static/css/main.b18fbf6f.chunk.css"
+  },
+  {
+    "revision": "bffc237a26f2f6b523ea",
+    "url": "/pomodoro-clock/static/js/2.f968c29e.chunk.js"
+  },
+  {
+    "revision": "960bf991070e212c4203",
+    "url": "/pomodoro-clock/static/js/main.9f701935.chunk.js"
+  },
+  {
+    "revision": "e09c61168d95896a8104",
+    "url": "/pomodoro-clock/static/js/runtime~main.828b5792.js"
+  },
+  {
+    "revision": "006ba76bd9521aa20e9cd79423eaaf55",
+    "url": "/pomodoro-clock/static/media/beep.006ba76b.mp3"
+  }
+]);
