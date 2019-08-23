6d635ca75b90c877a99080db55301af83e47b306
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2019-08-23 15:01:35 +0500

    Updates

--- precache-manifest.effbae2dd95b6f4f3d4e53b7fc102b8c.js
+++ precache-manifest.effbae2dd95b6f4f3d4e53b7fc102b8c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41a4280c1be4dc3eb481c7d205dc0f70",
+    "revision": "5cd59245d21eb622d0e08bc7ed339af7",
     "url": "/pomodoro-clock/index.html"
   },
   {
-    "revision": "daf567085440fea5671a",
+    "revision": "7b3d3878d085661361ab",
     "url": "/pomodoro-clock/static/css/main.58099c8c.chunk.css"
   },
   {
-    "revision": "bffc237a26f2f6b523ea",
-    "url": "/pomodoro-clock/static/js/2.f968c29e.chunk.js"
+    "revision": "54128e6fd4c0dd75e1f7",
+    "url": "/pomodoro-clock/static/js/2.3fd46505.chunk.js"
   },
   {
-    "revision": "daf567085440fea5671a",
-    "url": "/pomodoro-clock/static/js/main.74269f62.chunk.js"
+    "revision": "7b3d3878d085661361ab",
+    "url": "/pomodoro-clock/static/js/main.9c90adc2.chunk.js"
   },
   {
     "revision": "e09c61168d95896a8104",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-clock/precache-manifest.1351b5a7603cf53f2ec8482b99d267b4.js"
+  "/pomodoro-clock/precache-manifest.effbae2dd95b6f4f3d4e53b7fc102b8c.js"
 );
 
 self.addEventListener('message', (event) => {
